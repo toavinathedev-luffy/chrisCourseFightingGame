8c0eaebadb7b6a48124f88fad8600afe38a085e0
toavina <toavinathefirst@gmail.com>
2025-06-12 23:45:13 +0300

    draw player in rectangle form and ennemy finished

--- main.js
+++ main.js
@@ -10,10 +10,18 @@ canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 //Create Player abd ennemies
 class Sprite {
   /**
-   * @param {number} position
+   * @param {{x:number,y:number}} position
    */
   constructor(position) {
     this.position = position;
   }
+  draw() {
+    canvasContext.fillStyle = "red";
+    canvasContext.fillRect(this.position.x, this.position.y, 50, 150);
+  }
 }
-const player = new Sprite();
+const player = new Sprite({ x: 0, y: 0 });
+player.draw();
+const ennemy = new Sprite({ x: 400, y: 0 });
+ennemy.draw();
+console.log(player.position.x);
