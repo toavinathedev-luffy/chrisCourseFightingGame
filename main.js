e8d1a22e821bf58291e31aaf5a0b50a9387fcf15
toavina <toavinathefirst@gmail.com>
2025-06-12 23:30:08 +0300

    setup project

---
+++ main.js
@@ -0,0 +1,11 @@
+import "./style.css";
+//setup project
+/**  @type {HTMLCanvasElement}  */
+const canvas = document.querySelector("canvas");
+const canvasContext = canvas.getContext("2d");
+canvas.width = 1024;
+canvas.height = 576;
+canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+
+//Create Player abd ennemies
+class Sprite {}
